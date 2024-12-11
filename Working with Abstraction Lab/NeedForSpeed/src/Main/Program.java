a58fc51d2ae8ccb846762eb3746fe1731d7c3e65
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2024-12-11 06:46:31 +0200

    Add class main

---
+++ Working with Abstraction Lab/NeedForSpeed/src/Main/Program.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Vehicle.RaceMotocycle;
+
+public class Program {
+
+	public static void main(String[] args) 
+	{
+		
+			RaceMotocycle cM = new RaceMotocycle(100,250);
+			cM.drive(400);
+			
+	}
+
+}
