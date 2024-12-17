06cf502e7e9538f2b4fd0200679bb218110b1a7b
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2024-12-17 20:26:10 +0200

    Add files via upload

---
+++ Interfaces and Abstraction/Problem 5 Telephony/.src/Program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import smartphone.CommandHeandler;
+
+public class Program {
+	
+	public static void main(String[] arg) 
+	{
+		
+		CommandHeandler cmH = new CommandHeandler();
+		cmH.start();
+			
+	}
+
+}
