b9348060d624dd6598958deaacfe6365672ddec6
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-01-03 21:53:28 +0200

    Add files via upload

---
+++ Encapsulation/Salary Increase/src/Program.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import Command.Command;
+
+
+public class Program {
+
+	public static void main(String[] args) throws IOException, Throwable {
+		
+		Command c = new Command();
+	    
+		c.Start();
+
+	}
+
+}
