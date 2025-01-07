2b2823bd3131f8f2645821cec6c74c244f6ea3e2
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-01-07 13:42:16 +0200

    Add files via upload

---
+++ Polymorphism/Polumorphism Lab/Math Operations/src/mathOperations/MathOperation.java
@@ -0,0 +1,33 @@
+package mathOperations;
+
+import mathOperationsInterfaces.IMathOperation;
+
+public class MathOperation  implements IMathOperation  {
+
+	public MathOperation() 
+	{
+		
+	}
+	
+	@Override
+	public int add(int a, int b) {
+		
+		return a +b;
+		
+	}
+
+	@Override
+	public int add(int a, int b, int c) {
+		
+		return a + b+ c;
+	
+	}
+
+	@Override
+	public int add(int a, int b, int c, int d) {
+		
+		return  a + b + c +d;
+		
+	}
+
+}
