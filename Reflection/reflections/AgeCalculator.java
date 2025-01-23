efc59b6f26b48cd14fa367724be1957819d064ea
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-01-23 13:46:21 +0200

    Add files via upload

---
+++ Reflection/reflections/AgeCalculator.java
@@ -0,0 +1,9 @@
+package reflections;
+
+public interface AgeCalculator {
+	
+	int getAgeComparedToAverageYears(int actual,int average);
+	
+	int getDifferenceBetweenTheYears(int oldAge,int youngAge);
+	
+}
---
+++ Reflection/reflections/ReflectionMainOverview.java
@@ -0,0 +1,13 @@
+package reflections;
+
+public class ReflectionMainOverview {
+	public ReflectionMainOverview() 
+	{
+		
+	}
+	
+	public void printHelloReflection() 
+	{
+		System.out.println("Hello reflection from SUPER CLASS");
+	}
+}
