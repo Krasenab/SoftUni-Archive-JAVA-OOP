c9d58c870905cbf68b235ea76d15fbcd25988201
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-02-06 13:43:27 +0200

    Add files via upload

---
+++ SOLID/Logger/src/interfaces/Appenders.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Appenders {
+	public void appendMessage(String dateTime);
+	
+}
---
+++ SOLID/Logger/src/interfaces/Layout.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Layout {
+	public String format(String dateTime,String reportLevel,String message);
+}
