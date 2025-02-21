a6a0fedf8e977e05a4a9c15ae62d1306e169baab
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-02-21 21:28:58 +0200

    Add files via upload

--- Design Patterns/Design Patterns Lab/Singleton/src/Main.java
+++ Design Patterns/Design Patterns Lab/Singleton/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import singletonInterfaces.SingletonConatiner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		 // only one instance
+	     SingletonConatiner iSingletonContainer = SingletonDataContainer.getInstance();
+		
+	     Map<String,Integer> cityAndPopulation = new HashMap<>();
+	     cityAndPopulation.put("Sofiq", 110000);
+	     cityAndPopulation.put("Plovdiv",700000000);
+	     
+	    
+	     System.out.println(iSingletonContainer.getPopulation(cityAndPopulation, "Sofiq"));
+	     
+	     
 	}
 
 }
--- Design Patterns/Design Patterns Lab/Singleton/src/SingletonDataContainer.java
+++ Design Patterns/Design Patterns Lab/Singleton/src/SingletonDataContainer.java
@@ -1,4 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class SingletonDataContainer {
+import singletonInterfaces.SingletonConatiner;
+
+public class SingletonDataContainer implements SingletonConatiner {
+
+	private static SingletonDataContainer instance;
+	private Map<String,Integer> capitals;
+	
+	private SingletonDataContainer() 
+	{
+		this.capitals = new HashMap<>();
+	}
+	public static SingletonDataContainer getInstance() 
+	{
+		if(instance!=null) 
+		{
+			return instance;
+		}
+		instance = new SingletonDataContainer();
+		return instance;
+	}
+	@Override
+	public int getPopulation(Map<String, Integer> capitals, String name) {
+		
+		int result = 0;
+		for(String cap:capitals.keySet()) 
+		{
+			if(cap.contains(name)) 
+			{
+				result = capitals.get(cap);
+			}
+			
+		}
+		return result;
+	}
 
 }
