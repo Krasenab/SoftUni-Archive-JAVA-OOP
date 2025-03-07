19dda5fb9903fca81c134372883ca045bc799b2b
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-03-07 13:30:59 +0200

    Add files via upload

--- Exams/Java-OOP -Retake-19-Dec-2020/src/Main.java
+++ Exams/Java-OOP -Retake-19-Dec-2020/src/Main.java
@@ -1,9 +1,30 @@
+import controllers.ControllerImpl;
+import onlineShop.Computer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		
+		ControllerImpl c = new ControllerImpl();
+		
+		System.out.println(c.addComputer("Laptop", 4, "Asus", "ROG", 700));
+		//System.out.print(c.addComputer("Tablet", 5, "Asus", "ROG", 700));
+		//System.out.println(c.addComputer("Laptop", 0, "Asus", "ROG", 700));
+		//System.out.println(c.addComputer("Laptop", 4, "Asus", "ROG", 700));
+		//System.out.println(c.addComputer("Laptop", 7, "Asus", "ROG", 0));
+		
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		System.out.println(c.addComponent(4, 3, "CentralProcessingUnit", "Intel","Xeon", 1600, 82, 10));
+		for(Computer co: c.computers) 
+		{
+			System.out.println("Id is:" + co.getId());
+		}
+		
 	}
 
 }
