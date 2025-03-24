6c0bb4ad846231ba4d0ce0e9ed9f90b65c905101
Penev Krasen <160084524+Krasenab@users.noreply.github.com>
2025-03-24 19:45:59 +0200

    Add files via upload

--- SOLID/Payment system/src/Main.java
+++ SOLID/Payment system/src/Main.java
@@ -1,4 +1,6 @@
+import payments.CreditCardPayment;
 import payments.DebitCardPayment;
+import payments.PayPal;
 import payments.PaymentProcessor;
 
 public class Main {
@@ -9,10 +11,17 @@ public class Main {
 		debitCard.setAccountQuatity(150000);
 		PaymentProcessor p = new PaymentProcessor(debitCard);
 		
+		CreditCardPayment creditCard = new CreditCardPayment("697 625 358 892","07/2025","199");
+		PaymentProcessor p1 = new PaymentProcessor(creditCard);
 		
 		
+		PayPal pay = new PayPal();
+		pay.setBalance(122222222);
 		System.out.println(p.pay(12000));
-			
-	}
+		System.out.println(p1.pay(200000));	
+		
+		System.out.println(pay.payment(124545));
+		
+	}	
 
 }
